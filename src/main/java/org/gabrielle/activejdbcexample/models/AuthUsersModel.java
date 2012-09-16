feb196e1bbd341e5f68fdabe7e87eff721122c75
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2012-09-15 23:48:56 -0300

    ADD: model declaration for table auth_users. ADD: Eclipse + Eclim project files.

---
+++ src/main/java/org/gabrielle/activejdbcexample/models/AuthUsersModel.java
@@ -0,0 +1,22 @@
+/**
+ * AuthUsersModel
+ *
+ * Model declaration for table auth_users.
+ *
+ * @author Ariel Gerardo Rios <mailto:ariel.gerardo.rios@gmail.com>
+ *
+ */
+
+package org.gabrielle.activejdbcexample.models;
+
+import org.javalite.activejdbc.Model;
+
+/** 
+ * Model for table "auth_users".
+ *
+ * @author Ariel Gerardo Ríos <mailto:ariel.gerardo.rios@gmail.com>
+ * @version 1.00    
+*/
+public class AuthUsersModel extends Model {}
+
+// vim:ft=java:
